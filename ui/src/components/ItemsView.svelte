4abd03a75866e4d670c2a0fb231fd9dd544d5308
Vincenzo Ciancia <vincenzoml@gmail.com>
2023-09-15 23:53:23 +0000

    WIP

--- ui/src/components/ItemsView.svelte
+++ ui/src/components/ItemsView.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import ItemView from './ItemView.svelte';
+	import ItemView from './ItemView.svelte'
 	import { getStore } from './Store'
-    const store = getStore()
+	const store = getStore()
 </script>
 
-<div style="display:flex;">
-    {#each Object.keys($store.openItems) as path}
-    <ItemView {path} item={$store.openItems[path]} />
-    {/each}
+<div style="display:flex;justify-content: left;flex-wrap: wrap;">
+	{#each Object.keys($store.openItems) as path}
+		<div style="margin: 10px;">
+			<ItemView {path} item={$store.openItems[path]} />
+		</div>
+	{/each}
 </div>
